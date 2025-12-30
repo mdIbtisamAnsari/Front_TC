f6563f0e8c5794505b753e02fcc687c4b7ee213a
Ibtisam <173330530+mdIbtisamAnsari@users.noreply.github.com>
2025-12-30 21:18:21 +0530

    feat: update NavBar component to fetch user details and display profile information

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,68 +7,79 @@ import axios from 'axios'
 
 axios.defaults.withCredentials = true
 
+
 const NavBar = () => {
   const navigate = useNavigate()
+  const [user, setUser] = useState()
 
   useEffect(() => {
     const currentUser = async () => {
       const response = await axios.get('http://localhost:3000/api/v1/users/userdetails')
-      .then((response) => { 
-        return response.data
-      })
-      .catch(async(err) => { 
-        const response2 = await axios.post('http://localhost:3000/api/v1/users/getaccesstoken')
-        .then(async (response2) => {
-          const userResponse = await axios.get('http://localhost:3000/api/v1/users/userdetails')
-          return userResponse.data
+        .then((response) => {
+          setUser(response.data.data)
         })
-        .catch((err) => {
-          navigate('/login')
+        .catch(async (err) => {
+          await axios.post('http://localhost:3000/api/v1/users/getaccesstoken')
+            .then(async () => {
+              const userResponse = await axios.get('http://localhost:3000/api/v1/users/userdetails')
+                .then((userResponse) => {
+                  setUser(userResponse.data.data)
+                })
+            })
         })
-      })
-    } 
+    }
     currentUser()
   }, [])
 
+
   return (
     <>
-     <nav className='navbar'>
-      <span className='navbar-logo-title'>
-        <Link to='/' className='navbar-logo-link'>
-          <img className='logo' src={logo}/>
-          <span className='title'>TuteConnect</span>
-        </Link>
-      </span>
-      <span className='navbar-links-container'>
-        <NavLink 
-          to='/' 
-          className={({ isActive }) => isActive ? 'navbar-link-active' : 'navbar-link'}
-        >
-          Home
-        </NavLink>
-        <NavLink 
-          to='/portal' 
-          className={({ isActive }) => isActive ? 'navbar-link-active' : 'navbar-link'}
-        >
-          Portal
-        </NavLink>
-        <NavLink 
-          to='/profile' 
-          className={({ isActive }) => isActive ? 'navbar-link-active' : 'navbar-link'}
-        >
-          Profile
-        </NavLink>
-        <NavLink 
-          to='/about' 
-          className={({ isActive }) => isActive ? 'navbar-link-active' : 'navbar-link'}
-        >
-          About
-        </NavLink>
-      </span>
-      <span className='navbar_profile'>
-        
-      </span>
-     </nav>
+      <nav className='navbar'>
+        <span className='navbar-logo-title'>
+          <Link to='/' className='navbar-logo-link'>
+            <img className='logo' src={logo} />
+            <span className='title'>TuteConnect</span>
+          </Link>
+        </span>
+        <span className='navbar-links-container'>
+          <NavLink
+            to='/'
+            className={({ isActive }) => isActive ? 'navbar-link-active' : 'navbar-link'}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to='/portal'
+            className={({ isActive }) => isActive ? 'navbar-link-active' : 'navbar-link'}
+          >
+            Portal
+          </NavLink>
+          <NavLink
+            to='/profile'
+            className={({ isActive }) => isActive ? 'navbar-link-active' : 'navbar-link'}
+          >
+            Profile
+          </NavLink>
+          <NavLink
+            to='/about'
+            className={({ isActive }) => isActive ? 'navbar-link-active' : 'navbar-link'}
+          >
+            About
+          </NavLink>
+        </span>
+        <span className='navbar_profile'>
+          {user ? (
+            <>
+              <img className='profile_photo' src={user?.profilePhoto} alt='profile_photo' />
+              {user?.userName}
+            </>
+          ) :
+            (<>
+              <Link to='/login' className='navbar-link'>Login</Link>
+              <Link to='/register' className='navbar-link'>Signup</Link>
+            </>)}
+        </span>
+      </nav>
     </>
   )
 }
