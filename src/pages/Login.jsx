4bf454d3b5a7284b811fa56c3c34126cd9c0b3bc
Ibtisam <173330530+mdIbtisamAnsari@users.noreply.github.com>
2026-01-22 17:37:20 +0530

    fix: correct spelling of registration background variable and enhance login form with password visibility toggle

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,27 @@
 import { useState } from 'react'
 import './login.css'
 import { loginUser } from "../api/login.api.js"
-import {useNavigate} from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
+import { GrView } from "react-icons/gr";
+import { GrHide } from "react-icons/gr";
 
 
 const Login = () => {
   const navigate = useNavigate()
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [showPassword, setShowPassword] = useState(false)
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
     await loginUser(email, password)
     await navigate('/')
     window.location.reload()
   }
+
+  const handleShow = () => {
+    setShowPassword(!showPassword);
+  };
 
   return (
     <div className='login-container'>
@@ -23,26 +30,29 @@ const Login = () => {
         <form onSubmit={handleSubmit}>
           <div className='form-group'>
             <label>Email Address</label>
-            <input 
-              type="email" 
-              value={email} 
+            <input
+              type="email"
+              value={email}
               onChange={(e) => setEmail(e.target.value)}
               placeholder="Enter your email"
-              required 
+              required
             />
           </div>
           <div className='form-group'>
             <label>Password</label>
-            <input 
-              type="password" 
-              value={password} 
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Enter your password"
-              required 
-            />
+            <div className='pass-field'>
+              <input
+                type={showPassword ? 'text' : 'password'}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Enter your password"
+                required
+              />
+              <span onClick={handleShow} className='show'>{showPassword ? <GrView/> : <GrHide/>}</span>
+            </div>
           </div>
           <button type="submit" className='login-btn'>Sign In</button>
-          <br/><br/><br/>
+          <br /><br /><br />
           <div>Don't have an account <a href='/register'>login</a></div>
         </form>
       </div>
