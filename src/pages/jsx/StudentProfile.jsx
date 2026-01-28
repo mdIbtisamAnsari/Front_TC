5154e30608d9a295ec3b8fe241396de688e0017e
Ibtisam <173330530+mdIbtisamAnsari@users.noreply.github.com>
2026-01-29 01:14:16 +0530

    feat: enhance StudentProfile layout with improved post structure and styling

--- src/pages/jsx/StudentProfile.jsx
+++ src/pages/jsx/StudentProfile.jsx
@@ -20,20 +20,26 @@ const StudentProfile = () => {
 
     return (
         <>
-            <h3>Your Posts</h3>
+            <h1>Your Posts</h1>
             <div className='post-container'>
                 {posts?.map((post) => (
                     <div key={post._id} className='posts'>
-                        <div className='post-right'>
-                            {/* <img src={post.studentInfo.profilePhoto} className='post-image'/> */}
-                            <h3>{post.studentInfo.fullName}</h3>
-                            <p>{post.studentQualification}</p>
-                            <p>{post.selectedCategory}</p>
-                            <p>{post.selectedSubject}</p>
+                        <div className="post-top">
+                            <img src={post.studentInfo.profilePhoto} className='post-image' />
+                            <h3 className='post-student-name'>{post.studentInfo.fullName}</h3>
                         </div>
-                        <div className='post-left'>
-                            <p>{post.requirement}</p>
-                            <p>{post.offer}</p>
+
+                        <div className="post-bottom">
+                            <div className='post-right'>
+
+                                <p>{post.studentQualification}</p>
+                                <p>{post.selectedCategory}</p>
+                                <p>{post.selectedSubject}</p>
+                            </div>
+                            <div className='post-left'>
+                                <p>{post.requirement}</p>
+                                <p>{post.offer}</p>
+                            </div>
                         </div>
                     </div>
                 ))}
